3293ad088b52960db71a2c4a938752e469755aa9
madol-abraham <m.madol@alustudent.com>
2023-07-14 03:54:37 -0700

    print_list_integer.py

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def element_at(my_list, idx):
+    """Print a list of integers."""
+    for i in range(len(my_list)
+            print("{:d}".format(my_list[i]))
