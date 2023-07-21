1dc4a228b643f8d9075d3cac2bd513719fee1d65
madol-abraham <m.madol@alustudent.com>
2023-07-21 09:34:59 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-#Write an empty class Square that defines a square:
 class square:
-   pass
-
-   my_square = Square()
-   my_square.__dict__ = square()
-   print(type(my_square))
-   print(my_square.__dict__)
+    # Empty class square
+    pass
 
