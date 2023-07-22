edf993f06e82e87e8db42225a48570774f381b72
madol-abraham <m.madol@alustudent.com>
2023-07-22 12:02:28 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-"""Defining a class square"""
+"""Defining a class square."""
 
 
 class Square:
-    """Representing an empty square"""
+    """Representing an empty square."""
     pass
 
