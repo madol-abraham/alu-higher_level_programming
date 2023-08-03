b5cc8ee4f410cafa341bee7675b6632863538d65
madol-abraham <m.madol@alustudent.com>
2023-08-03 12:25:48 -0700

    1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,7 +1,3 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost';
+#!/bin/bash
+# This script takes in a URL, sends GET request and displays body of response
+curl -sL "$1"
