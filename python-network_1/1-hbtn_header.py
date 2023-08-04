9cc9442180029d2cbf97b6ec07ac6366630f67da
madol-abraham <m.madol@alustudent.com>
2023-08-04 10:11:15 +0000

    2-post_email.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""A script that takes in a URL,
-Sends a request to the URL,
-And displays the value of the X-Request-Id variable found in the header ofthe response.
+""" Script that takes in a URL and an email, sends a POST request
+ And displays the body of the response
 """
 
 import sys
+import urllib.parse
 import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url)
+    request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as resp:
-        print(dict(resp.headers).get("X-Request-Id"))
+        print(resp.read().decode("utf-8"))
