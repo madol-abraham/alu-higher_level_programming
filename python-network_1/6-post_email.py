fde5c1825dddf8100c56b6bea90ed3a0ff51c7d6
madol-abraham <m.madol@alustudent.com>
2023-08-04 09:22:24 +0000

    6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and finally displays the body of the response
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
+
+    print(req.text)
