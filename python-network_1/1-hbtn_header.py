7cfabae017c351e79af26faa7c60544c13f54816
madol-abraham <m.madol@alustudent.com>
2023-08-04 10:15:21 +0000

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-""" Script that takes in a URL and an email, sends a POST request
- And displays the body of the response
 """
+Script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
 
-import sys
-import urllib.parse
-import urllib.request
+Usage: ./1-hbtn_header.py <URL>
+"""
+from sys import argv
+from urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    url = argv[1]
+    req = Request(url)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as resp:
-        print(resp.read().decode("utf-8"))
+    with urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
